3a360f9f29a817d402483d7db571cbe4515d411d
mjsgreen01 <mjsgreen01@gmail.com>
2015-07-30 16:47:49 -0700

    [feat] drunken-blur on random intervals

--- public/js/instatrip.map.js
+++ public/js/instatrip.map.js
@@ -2,11 +2,38 @@ angular.module('instatrip.map',[])
   .controller('mapCtrl', mapCtrl);
 
 function mapCtrl ($scope, Getdata, $rootScope){
+  
+  function randomIntFromInterval(min,max){
+      return Math.floor(Math.random()*(max-min+1)+min);
+  }
+
+  function setRandomInterval(){
+    var bodyEl = angular.element( document.querySelector( 'body' ) );
+    setTimeout(function(){
+      bodyEl.addClass('youreDrunk');
+      setTimeout(function(){
+        bodyEl.removeClass('youreDrunk');
+      }, 1500);
+      setRandomInterval();
+    }, randomIntFromInterval(9000,17000));
+  }
+
+  setRandomInterval();
+  // setInterval(function(){
+  //   var bodyEl = angular.element( document.querySelector( 'body' ) );
+  //   bodyEl.addClass('youreDrunk');
+  //   setTimeout(function(){
+  //     bodyEl.removeClass('youreDrunk');
+  //   }, 1500);
+    
+  // }, 10000);
+  
+
   $scope.getmap = Getdata.getmap;
 
   $scope.makeMap = function(){
     Getdata.getmap($rootScope.start, $rootScope.end, $rootScope.travelMethod);
-  }
+  };
 
   $scope.makeMap();
 }
