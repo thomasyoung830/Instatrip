f7d6a5e07b6ef9830d30b1c786a2e3d821b5f5cc
Thomas <thomas.c.young830@gmail.com>
2015-07-31 16:37:05 -0700

    [feat] added uber to router

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,6 +1,7 @@
 var foursquare = require('../api/foursquare');
 var instagram = require('../api/instagram');
 var maps = require('../api/maps');
+var uber = require('../api/uber');
 var express = require('express');
 var router = express.Router();
 
@@ -18,7 +19,30 @@ router.post('/', function(req, res) {
 
     return foursquare.get_foursquare_data_for_array_of_points(points);
   }).then(function(data) {
-    return foursquare.choose_foursquare_venues(data.map(foursquare.filter_foursquare_data));
+    var venue_ids = {};
+
+    var fourSquareData = data.map(function(venues) {
+      for(var i = 0; i < venues.length; i++) {
+        if (venue_ids[venues[i].venue.id]) {
+          continue;
+        }
+
+        venue_ids[venues[i].venue.id] = true;
+
+        return {
+          'name': venues[i].venue.name,
+          'coordinates': {
+            'lat': venues[i].venue.location.lat,
+            'lng': venues[i].venue.location.lng
+          },
+          'address': venues[i].venue.location.formattedAddress.join(' '),
+          'foursquare_v2_id': venues[i].venue.id
+        };
+      }
+    });
+    console.log("data from 4square", fourSquareData);
+    return fourSquareData;
+
   }).then(function(data){
     console.log('right before calling instagram with: ', data);
     // // test-data
