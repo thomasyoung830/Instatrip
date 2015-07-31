bff9708e1464d79eb60e3f31c3e9b4d717f60aa1
Thomas <thomas.c.young830@gmail.com>
2015-07-31 11:48:27 -0700

    [feat] advances on uber API

--- api/uber.js
+++ api/uber.js
@@ -1,66 +1,66 @@
 var keys = require('../config.js');
 
-function Products(uber) {
-  this._uber = uber;
-  this.path = 'products';
-};
+// Uber API Constants
+var uberClientId = keys.UBER_CLINET_ID
+  , uberServerToken = keys.UBER_SERVER_TOKEN;
 
-module.exports = Products;
+// Create variables to store latitude and longitude
+var userLatitude
+  , userLongitude
+  , endLatitude = 40.7283405
+  , endLongitude = -73.994567;
 
-//get products based on location
-var get_products = function (lat, lng) {
-	
- "description": "The low-cost Uber",
-       "price_details": {
-          "distance_unit": "mile",
-          "cost_per_minute": 0.26,
-          "service_fees": [
-             {
-                "fee": 1.0,
-                "name": "Safe Rides Fee"
-             }
-          ],
-          "minimum": 5.0,
-          "cost_per_distance": 1.3,
-          "base": 2.2,
-          "cancellation_fee": 5.0,
-          "currency_code": "USD"
-       },
-       "image": "http://d1a3f4spazzrp4.cloudfront.net/car.jpg",
-       "display_name": "uberX",
-       "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d"
+// Create variable to store timer
+var timer;
 
-  };
+navigator.geolocation.watchPosition(function(position) {
+  // Update latitude and longitude
+  userLatitude = position.coords.latitude;
+  userLongitude = position.coords.longitude;
 
+  // Create timer 
+  // Once initialized, it will fire every 60 seconds as recommended by the Uber API
+  // We only create the timer after we've gotten the user's location for the first time 
+  if (typeof timer === typeof undefined) {
+    timer = setInterval(function() {
+        getEstimatesForUserLocation(userLatitude, userLongitude);
+    }, 60000);
 
-//get price based on location
-var get_price = function(start_lat, start_lng, end_lat, end_lng) {
-	return request({
-		'uri': "https://sandbox-api.uber.com/v1",
-		"prices": {
-      "product_id": "08f17084-23fd-4103-aa3e-9b660223934b",
-      "currency_code": "USD",
-      "display_name": "UberBLACK",
-      "estimate": "$23-29",
-      "low_estimate": 23,
-      "high_estimate": 29,
-      "surge_multiplier": 1,
-      "duration": 640,
-      "distance": 5.34
-    }
-  })
-};
-
-var get_time_estimate = function(query, callback) {
-	if (!query.start_latitude && !query.start_longitude) {
-    return callback(new Error('Invalid parameters'));
+    // Query Uber API if needed
+    getEstimatesForUserLocation(userLatitude, userLongitude);
   }
-  
-  return this._uber.get({ url: this.path + '/time', params: query }, callback);
-};
+});
 
-module.exports {
-	get_products: get_products,
-	get_price: get_price,
-	get_time_estimate: get_time_estimate
+function getEstimatesForUserLocation(latitude,longitude) {
+  https({
+    url: "https://api.uber.com/v1/estimates/price",
+    headers: {
+      Authorization: "Token " + uberServerToken
+    },
+    data: { 
+      start_latitude: latitude,
+      start_longitude: longitude,
+      end_latitude: endLatitude,
+      end_longitude: endLongitude
+    }
+    .then.function(res) {
+      console.log(JSON.stringify(res));
+
+      // 'res' is an object with a key containing an Array
+      var data = res["prices"]; 
+      if (typeof data != typeof undefined) {
+        // Sort Uber products by time to the user's location 
+        data.sort(function(t0, t1) {
+          return t0.duration - t1.duration;
+        });
+
+        // Update the Uber button with the shortest time
+        var shortest = data[0];
+        if (typeof shortest != typeof undefined) {
+          console.log("Updating time estimate...");
+          $("#time").html("IN " + Math.ceil(shortest.duration / 60.0) + " MIN");
+        }
+      }
+    }
+  });
 }
